8aff04f5a39d2066faed255b6140637212737899
Ian Kamar <ian.kamar@Outlook.com>
2023-07-15 13:10:33 -0400

    update my project

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,25 +1,23 @@
 // This function shows an error message for an invalid input element
-function showInputError(formElement, inputElement, errorMessage, config) {
-  const errorElement = formElement.querySelector(
-    "#" + inputElement.id + "-Error"
-  );
+const showInputError = (formElement, inputElement, errorMessage, config) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-Error`);
   inputElement.classList.add(config.inputErrorClass);
   errorElement.textContent = errorMessage;
   errorElement.classList.add(config.errorClass);
-}
+};
 
 // This function hides the error message for a valid input element
-function hideInputError(formElement, inputElement, config) {
-  const errorElement = formElement.querySelector(
-    "#" + inputElement.id + "-Error"
-  );
+const hideInputError = (formElement, inputElement, config) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-Error`);
+  console.log(inputElement);
   inputElement.classList.remove(config.inputErrorClass);
+  console.log(errorElement);
   errorElement.textContent = "";
   errorElement.classList.remove(config.errorClass);
-}
+};
 
 // This function checks the validity of an input element and shows or hides the error message accordingly
-function checkInputValidity(formElement, inputElement, config) {
+const checkInputValidity = (formElement, inputElement, config) => {
   if (!inputElement.validity.valid) {
     showInputError(
       formElement,
@@ -30,57 +28,27 @@ function checkInputValidity(formElement, inputElement, config) {
   } else {
     hideInputError(formElement, inputElement, config);
   }
-}
-
-// Set a custom validation message for the name input
-function setCustomValidationMessages() {
-  const profileNameInput = document.getElementById("profileNameInput");
-  const profileDescriptionInput = document.getElementById(
-    "profileDescriptionInput"
-  );
-
-  profileNameInput.setCustomValidity(
-    profileNameInput.value === "" ? "Please fill out the name field" : ""
-  );
-  profileDescriptionInput.setCustomValidity(
-    profileDescriptionInput.value === ""
-      ? "Please fill out the description field"
-      : ""
-  );
-}
-
-// Set a custom validation message for the title input
-function setCustomValidationMessages() {
-  const cardTitleInput = document.getElementById("cardTitleInput");
-  const cardImageInput = document.getElementById("cardImageInput");
-
-  cardTitleInput.setCustomValidity(
-    cardTitleInput.value === "" ? "Please fill out the field" : ""
-  );
-  cardTitleInput.setCustomValidity(
-    cardImageInput.value === "" ? "Please fill out the field" : ""
-  );
-}
+};
 
 // This function toggles the disabled state and the inactive class of the submit button based on the validity of the input elements
-function toggleButtonState(inputList, submitButton, config) {
-  let foundInvalid = false;
-  inputList.forEach(function (input) {
-    if (!input.validity.valid) {
-      foundInvalid = true;
-    }
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
   });
+};
 
-  submitButton.disabled = foundInvalid;
-  if (submitButton.disabled) {
+const toggleButtonState = (inputList, submitButton, config) => {
+  if (hasInvalidInput(inputList)) {
     submitButton.classList.add(config.inactiveButtonClass);
+    submitButton.disabled = true;
   } else {
     submitButton.classList.remove(config.inactiveButtonClass);
+    submitButton.disabled = false;
   }
-}
+};
 
 // This function sets the event listeners for each input element and the submit button
-function setEventListeners(formElement, config) {
+const setEventListeners = (formElement, config) => {
   const inputList = Array.from(
     formElement.querySelectorAll(config.inputSelector)
   );
@@ -93,20 +61,19 @@ function setEventListeners(formElement, config) {
       toggleButtonState(inputList, submitButton, config);
     });
   });
-}
+};
 
 // This function enables the validation for all the forms in the document
-function enableValidation(config) {
+const enableValidation = (config) => {
   const formList = Array.from(document.querySelectorAll(config.formSelector));
-  formList.forEach(function (formElement) {
+  formList.forEach((formElement) => {
     formElement.addEventListener("submit", function (e) {
       e.preventDefault();
     });
 
-    setCustomValidationMessages();
     setEventListeners(formElement, config);
   });
-}
+};
 
 const configObjects = {
   formSelector: ".modal__form",
