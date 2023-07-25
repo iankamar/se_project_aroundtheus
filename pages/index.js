4c7f74139e845ed5f4ac76bcc3a4d0da8efd0ae0
IanKamar <ian.kamar@outlook.com>
2023-07-25 06:19:42 -0400

    update my project

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 // Import necessary modules and constants
 import FormValidator from "../components/FormValidator.js";
 import { initialCards } from "../utils/utils.js";
+import {closeModalWindow,
+openModalWindow,
+addModalEventListener,} from "../utils/utils.js";
 
 // DOM elements
 const cardsWrap = document.querySelector("#cardList");
@@ -149,36 +152,6 @@ cardCloseButton.addEventListener("click", () => closeModalWindow(cardAddModal));
 
 profileForm.addEventListener("submit", handleProfileFormSubmit);
 cardAddForm.addEventListener("submit", handleCardFormSubmit);
-
-// Modal Window Functions with Keydown Event
-function closeModalEscape(e) {
-  if (e.key === "Escape" || e.code === "Escape") {
-    const openedModal = document.querySelector(".modal_opened");
-    closeModalWindow(openedModal);
-  }
-}
-
-function openModalWindow(modal) {
-  modal.classList.add("modal_opened");
-  document.addEventListener("keydown", closeModalEscape);
-}
-
-function closeModalWindow(modal) {
-  modal.classList.remove("modal_opened");
-  document.removeEventListener("keydown", closeModalEscape);
-}
-
-// Close a Modal Window by Clicking on Specific Elements
-function addModalEventListener(modalElement, closeElements) {
-  modalElement.addEventListener("mousedown", (e) => {
-    if (
-      e.target.classList.contains("modal") ||
-      closeElements.some((element) => e.target.classList.contains(element))
-    ) {
-      closeModalWindow(modalElement);
-    }
-  });
-}
 
 addModalEventListener(profileEditModal, ["modal__close"]);
 addModalEventListener(cardAddModal, ["modal__close"]);
--- utils/utils.js
+++ utils/utils.js
@@ -79,3 +79,41 @@ export const settings = {
   inputErrorClass: "modal__input_type_error",
   errorClass: "modal__error_visible",
 };
+
+// Modal Window Functions with Keydown Event
+function closeModalEscape(e) {
+  if (e.key === "Escape" || e.code === "Escape") {
+    const openedModal = document.querySelector(".modal_opened");
+    closeModalWindow(openedModal);
+  }
+}
+
+function openModalWindow(modal) {
+  modal.classList.add("modal_opened");
+  document.addEventListener("keydown", closeModalEscape);
+}
+
+function closeModalWindow(modal) {
+  modal.classList.remove("modal_opened");
+  document.removeEventListener("keydown", closeModalEscape);
+}
+
+// Close a Modal Window by Clicking on Specific Elements
+function addModalEventListener(modalElement, closeElements) {
+  modalElement.addEventListener("mousedown", (e) => {
+    if (
+      e.target.classList.contains("modal") ||
+      closeElements.some((element) => e.target.classList.contains(element))
+    ) {
+      closeModalWindow(modalElement);
+    }
+  });
+}
+// Export all utility functions together
+
+export {
+  closeModalEscape,
+  openModalWindow,
+  closeModalWindow,
+  addModalEventListener,
+};
