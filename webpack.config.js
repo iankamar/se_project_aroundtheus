20f2986815ac3c7e276f674b8117df2edea59c13
IanKamar <ian.kamar@outlook.com>
2023-08-02 16:52:52 -0400

    update my project

--- webpack.config.js
+++ webpack.config.js
@@ -38,11 +38,7 @@ module.exports = {
           MiniCssExtractPlugin.loader,
           {
             loader: "css-loader",
-            options: {
-              importLoaders: 1,
-            },
           },
-          "postcss-loader",
         ],
       },
       {
