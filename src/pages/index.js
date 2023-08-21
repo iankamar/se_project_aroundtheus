ce8f56549a16d080b6e379d08979c9b32b2d4609
Ian Kamar <ian.kamar@outlook.com>
2023-08-21 13:59:59 -0400

    update my project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+/*
 // Import necessary modules and constants
 import FormValidator from "../components/FormValidator.js";
 import "../pages/index.css";
@@ -64,6 +65,7 @@ const section = new Section(
   { items: initialCards, renderer: (item) => renderCard(item, section) },
   ".cards"
 );
+*/
 /*
 // create an instance of api class
 // Use fetch API
@@ -74,7 +76,7 @@ fetch("https://around-api.en.tripleten-services.com/v1",{
   },
 });
 */
-
+/*
 // Function to render a card using the Card class
 function renderCard(cardData, section) {
   const card = new Card(cardData, "#cardTemplate", handlePreviewImage);
@@ -154,3 +156,202 @@ const cardPreviewModalInstance = new ModalWithImage({
 profileEditModalInstance.setEventListeners();
 cardFormModalInstance.setEventListeners();
 cardPreviewModalInstance.setEventListeners();
+*/
+
+
+// Import necessary modules and constants
+import FormValidator from "../components/FormValidator.js";
+import "../pages/index.css";
+import {
+  config,
+  initialCards,
+  validationConfig,
+  selectors,
+  settings,
+} from "../utils/constants.js";
+
+import UserInfo from "../components/UserInfo.js";
+import Card from "../components/Card.js";
+import ModalWithForm from "../components/ModalWithForm.js";
+import ModalWithImage from "../components/ModalWithImage.js";
+import Section from "../components/Section.js";
+import Modal from "../components/Modal.js";
+import Api from "../utils/Api.js";
+
+// DOM elements
+const cardsWrap = document.querySelector("#cardList");
+const profileEditButton = document.querySelector("#profileEditButton");
+const profileEditModal = document.querySelector("#profileEditModal");
+const profileCloseButton = document.querySelector("#profileCloseButton");
+const profileName = document.querySelector("#profileName");
+const profileDescription = document.querySelector("#profileDescription");
+const profileNameInput = document.querySelector("#profileNameInput");
+const profileDescriptionInput = document.querySelector(
+  "#profileDescriptionInput"
+);
+
+const profileForm = document.querySelector("#profileEditForm");
+const profileAddButton = document.querySelector("#profileAddButton");
+const cardAddButton = document.querySelector("#cardAddButton");
+const cardTemplate = document.querySelector("#cardTemplate");
+const cardAddModal = document.querySelector("#cardAddModal");
+const cardCloseButton = document.querySelector("#cardCloseButton");
+const cardTitleInput = document.querySelector("#cardTitleInput");
+const cardImageInput = document.querySelector("#cardImageInput");
+const cardPreviewModal = document.querySelector("#cardPreviewModal");
+const cardPreviewCloseButton = document.querySelector(
+  "#modalCardPreviewCloseButton"
+);
+
+const cardImage = cardPreviewModal.querySelector("#modalPreviewImage");
+const cardCaption = cardPreviewModal.querySelector("#modalCaption");
+const cardModalButton = cardPreviewModal.querySelector("#cardModalButton");
+
+const api = new Api({
+  baseUrl:"https://around-api.en.tripleten-services.com/v1/users/me",
+  headers: {
+    authorization:  "68a8781d-0ef6-4f31-8a00-f95473676b23",
+    "Content-Type": "application/json"
+  }
+})
+
+// Instance of the UserInfo class
+const userInfo = new UserInfo({
+  userNameSelector: ".profile__name",
+  userDescriptionSelector: ".profile__description",
+  userImageSelector: ".profile__image",
+});
+
+// Create and enable form validators for editing profile and adding cards
+const editProfileValidator = new FormValidator(config, profileForm);
+editProfileValidator.enableValidation();
+const editCardValidator = new FormValidator(config, cardAddModal);
+editCardValidator.enableValidation();
+
+// Create an instance of the Section class
+const section = new Section(
+  { items: initialCards, renderer: (item) => renderCard(item, section) },
+  ".cards"
+);
+
+// Fetch user data and cards from the server
+fetchUserDataAndCards();
+
+// Function to fetch user data and cards from the server
+function fetchUserDataAndCards() {
+  // Fetch user data
+  fetch("https://around-api.en.tripleten-services.com/v1/users/me", {
+    headers: {
+      authorization: "68a8781d-0ef6-4f31-8a00-f95473676b23", // Replace with your actual authorization token
+    },
+  })
+    .then((response) => response.json())
+    .then((userData) => {
+      // Update user info using the fetched data
+      userInfo.setUserInfo({
+        userName: userData.name,
+        userDescription: userData.about,
+        userAvatar: userData.avatar,
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching user data:", error);
+    });
+
+  // Fetch cards data
+  fetch("https://around-api.en.tripleten-services.com/v1/cards", {
+    headers: {
+      authorization: "68a8781d-0ef6-4f31-8a00-f95473676b23", // Replace with your actual authorization token
+    },
+  })
+
+    .then((response) => response.json())
+    .then((cardsData) => {
+      // Render fetched cards
+      cardsData.forEach((cardData) => renderCard(cardData, section));
+    })
+    .catch((error) => {
+      console.error("Error fetching cards data:", error);
+    });
+}
+
+// Function to render a card using the Card class
+function renderCard(cardData, section) {
+  const card = new Card(cardData, "#cardTemplate", handlePreviewImage);
+  const cardElement = card.getView();
+  section.addItem(cardElement);
+}
+
+// Render initial cards
+section.renderItems();
+
+// Event handler for profile form submission
+function handleProfileFormSubmit(inputValues) {
+  console.log(inputValues);
+  userInfo.setUserInfo(inputValues);
+  profileEditModalInstance.close();
+}
+
+function fillProfileForm() {
+  const { userName, userDescription } = userInfo.getUserInfo();
+  // Set the input values in the profile form
+  profileNameInput.value = userName;
+  profileDescriptionInput.value = userDescription;
+}
+
+function handleCardFormSubmit(inputValues) {
+  console.log(inputValues);
+  // Get input values from the form
+  const cardTitle = inputValues["card-title-input"];
+  const cardImage = inputValues["card-image-input"];
+
+  // Card data object
+  const cardData = {
+    name: cardTitle,
+    link: cardImage,
+  };
+
+  // Render the new card
+  renderCard(cardData, section);
+
+  // Close the card add modal
+  cardFormModalInstance.close();
+}
+
+// Event handler for previewing an image in a modal
+function handlePreviewImage(cardData) {
+  cardPreviewModalInstance.open(cardData);
+}
+
+// Event listeners
+profileEditButton.addEventListener("click", () => {
+  editCardValidator.resetValidation();
+  fillProfileForm();
+  profileEditModalInstance.open();
+});
+
+// Event listeners
+profileAddButton.addEventListener("click", () => {
+  editCardValidator.resetValidation();
+  cardFormModalInstance.open();
+});
+
+const profileEditModalInstance = new ModalWithForm({
+  modalSelector: "#profileEditModal",
+  handleFormSubmit: handleProfileFormSubmit,
+});
+
+const cardFormModalInstance = new ModalWithForm({
+  modalSelector: "#cardAddModal",
+  handleFormSubmit: handleCardFormSubmit,
+});
+
+const cardPreviewModalInstance = new ModalWithImage({
+  modalSelector: "#cardPreviewModal",
+  handleFormSubmit: handleCardFormSubmit,
+});
+
+profileEditModalInstance.setEventListeners();
+cardFormModalInstance.setEventListeners();
+cardPreviewModalInstance.setEventListeners();
+
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,60 +1,52 @@
 // Token = "68a8781d-0ef6-4f31-8a00-f95473676b23"
 
 export default class Api {
-   constructor(config) {
-    this.baseUrl = config.baseUrl;
-    this.headers = config.headers;
-   }
+  constructor(baseUrl, headers) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
 
-   _checkResponse(res) {
+  _checkResponse(res) {
     if (res.ok) {
       return res.json();
     }
     return Promise.reject(`Error: ${res.status}`);
-   }
+  }
 
-   getUserInfo() {
+  getUserInfo() {
     return fetch(`${this._baseUrl}/users/me`, {
       headers: this._headers,
-    })
-    .then(this._checkResponse);
-   }
+    }).then(this._checkResponse);
+  }
 
-   getInitialCards() {
+  getInitialCards() {
     return fetch(`${this._baseUrl}/cards`, {
       headers: this._headers,
-    })
-    .then(this._checkResponse);
-   }
+    }).then(this._checkResponse);
+  }
 
-   setProfileImage(baseUrl) {
-    return fetch(`${this._baseUrl}/users/me/profileImage`,{
+  setProfileImage(baseUrl) {
+    return fetch(`${this._baseUrl}/users/me/profileImage`, {
       method: "PATCH",
-      headers: this.headers,
+      headers: this._headers,
       body: JSON.stringify({
         profileImage: baseUrl,
       }),
-    })
-    .then(this._checkResponse);
-   }
+    }).then(this._checkResponse);
+  }
 
-   getWebpageInfo() {
-    return Promise.all([this.getInitialCards(), this.getUserInfo()]);
-   }
-
-   editUserInfo(data) {
-    return fetch(`${this._baseurl}/users/me`, {
+  editUserInfo(data) {
+    return fetch(`${this._baseUrl}/users/me`, {
       method: "PATCH",
       headers: this._headers,
       body: JSON.stringify({
         name: data.name,
-        about: data.about,
+        description: data.description,
       }),
-    })
-    .then(this._checkResponse);
-   }
+    }).then(this._checkResponse);
+  }
 
-   editProfile({data}) {
+  editProfile(data) {
     return fetch(`${this._baseUrl}/users/me`, {
       method: "PATCH",
       headers: this._headers,
@@ -62,44 +54,38 @@ export default class Api {
         name: data.name,
         about: data.link,
       }),
-    })
-    .then(this._checkResponse);
-   }
+    }).then(this._checkResponse);
+  }
 
-   addCard ({data}) {
-    return fetch (`${this._baseUrl}/cards`, {
+  addCard(data) {
+    return fetch(`${this._baseUrl}/cards`, {
       method: "POST",
       headers: this._headers,
       body: JSON.stringify({
         name: data.name,
         link: data.link,
       }),
-    })
-    .then(this._checkResponse);
-   };
+    }).then(this._checkResponse);
+  }
 
-   deleteCard(cardId) {
+  deleteCard(cardId) {
     return fetch(`${this._baseUrl}/cards/${cardId}`, {
       method: "DELETE",
       headers: this._headers,
-    })
-    .then(this._checkResponse);
-   }
+    }).then(this._checkResponse);
+  }
 
-   addLike(cardId) {
+  addLike(cardId) {
     return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
       method: "PUT",
       headers: this._headers,
-    })
-    .then(this._checkResponse);
-   }
+    }).then(this._checkResponse);
+  }
 
-   removeLike(cardId) {
-    return fetch (`${this._baseUrl}/cards/likes/${cardId}`, {
+  removeLike(cardId) {
+    return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
       method: "DELETE",
       headers: this._headers,
-    })
-    .then(this._checkResponse);
-   }
-
+    }).then(this._checkResponse);
+  }
 }
